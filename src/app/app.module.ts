6d7c4ae30af7ae57d7cd7be5cbedd4ef6e349e00
Noelia-ux <noelia.cardosorubio@gmail.com>
2024-08-04 16:50:35 +0200

    commit fin parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
 import { HeroModule } from './heroes/hero.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/components/counter/counter.component';
+
 
 
 
@@ -16,6 +18,7 @@ import { CounterComponent } from './counter/components/counter/counter.component
     BrowserModule,
     CounterModule,
     HeroModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { OutletContext } from '@angular/router';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [{
+    name:'Trunk',
+    power: 10
+  }]
+
+//onDeleteId = Index value= number,
+
+@Output()
+public onDelete: EventEmitter<number> = new EventEmitter();
+
+
+onDeteteCharacter(index:number):void{
+
+   this.onDelete.emit(index);
+
+  }
+
+}
